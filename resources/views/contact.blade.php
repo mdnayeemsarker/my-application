0f81315f1dc2fef58da031a06f3ac58f28d99b5f
Nayeemasha703 <nayeemasha703@gmail.com>
2022-04-18 21:34:49 +0600

    Basic Route

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Conact Us</title>
+</head>
+
+<body>
+    <a href="{{ url('/') }}">Home</a>
+    <h2>This is an Contact Us Page</h2>
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+
+
+
+
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
